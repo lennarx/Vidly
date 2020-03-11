026e37f42f07f6e5b351c96b4fb5a03fa8d7e9f3
Franco Redoni <Franco.Redoni@ey.com>
2020-03-11 20:56:27 -0300

    Initial Commit

---
+++ Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TIdentity.Startup))]
+namespace TIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
